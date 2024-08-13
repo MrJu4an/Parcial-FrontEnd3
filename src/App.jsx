186df0cbce8dc40a7da4eb5ae97d3e380d6e0631
mrju4an <jsebas3102@hotmail.com>
2024-08-12 20:58:39 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App' >
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
